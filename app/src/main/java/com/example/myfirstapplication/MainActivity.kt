97bfbdc7221d5fcaa39d5c8ff5966e482a15fb04
yuxi mao <31374138+yuximao@users.noreply.github.com>
2020-12-15 15:15:25 -0500

    intent and toast

--- app/src/main/java/com/example/myfirstapplication/MainActivity.kt
+++ app/src/main/java/com/example/myfirstapplication/MainActivity.kt
@@ -1,11 +1,47 @@
 package com.example.myfirstapplication
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import android.view.Gravity
+import android.widget.Button
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.custom_toast.*
+import kotlinx.android.synthetic.main.custom_toast.view.*
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        normal_toast.setOnClickListener {
+            val text = "Hello toast!"
+            val duration = Toast.LENGTH_SHORT
+            val toast = Toast.makeText(applicationContext, text, duration)
+            toast.show()
+        }
+        normal_position_toast.setOnClickListener {
+            val text = "Hello toast2!"
+            val duration = Toast.LENGTH_SHORT
+            val toast = Toast.makeText(applicationContext, text, duration)
+            toast.setGravity(Gravity.CENTER_VERTICAL, 0, 50)
+            toast.show()
+        }
+//        custom_toast.setOnClickListener {
+//            val v = layoutInflater.inflate(R.layout.custom_toast, custom_toast_container, false)
+//            v.text_view.text = getString(R.string.msg)
+//            with(Toast(this)) {
+//                duration = Toast.LENGTH_LONG
+//                view = v
+//                show()
+//            }
+//        }
+//        button1.setOnClickListener{
+//            val intent = Intent(applicationContext, SecondActivity::class.java)
+//            startActivity(intent)
+//        }
+
     }
+
 }
---
+++ app/src/main/java/com/example/myfirstapplication/SecondActivity.kt
@@ -0,0 +1,14 @@
+package com.example.myfirstapplication
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+
+class SecondActivity: AppCompatActivity()  {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+
+    }
+}
